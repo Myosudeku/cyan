db4aac3c45ab1f1476f6f77837acc4afbc61a7bb
Myosudeku <andy.snyder1@aol.com>
2016-01-06 17:02:30 -0500

    Update attack.lua

--- attack.lua
+++ attack.lua
@@ -1,1 +1,16 @@
+damageFactorMax = math.random(150)
+damageFactor = math.random(1,damageFactorMax)
+turn = 1 -- 1 for Player to Enemy Attack, 2 for Enemy to Player
 
+if turn == 1 then
+    enemyHP = enemyHP - damageFactor * playerAtk / enemyDef
+    damage = enemyHP
+    return damage
+end
+
+
+if turn == 2 then
+    playerHP = playerHP - damageFactor * enemyAtk / playerDef
+    damage = playerHP
+    return damage
+end
